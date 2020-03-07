d7262f231c8de49c7011aecdae799d970805270e
Alex Lalejini <amlalejini@gmail.com>
2020-03-07 15:43:01 -0500

    remove bias toward down-regulation by adding duplicate regulation instructions that multiply by -1

--- source/AltSignalWorld.h
+++ source/AltSignalWorld.h
@@ -42,6 +42,7 @@
 #include "Event.h"
 
 #include "reg_ko_instr_impls.h"
+// #include "regulators.h"
 
 // TODO - use compile args!
 namespace AltSignalWorldDefs {
@@ -113,7 +114,10 @@ namespace AltSignalWorldDefs {
       emp::AdditiveCountdownRegulator<>,
     std::conditional<STRINGVIEWIFY(MATCH_REG) == "mult",
       emp::MultiplicativeCountdownRegulator<>,
-      std::enable_if<false>
+    // std::conditional<STRINGVIEWIFY(MATCH_REG) == "exp",
+    //   emp::ExponentialCountdownRegulator<>,
+    std::enable_if<false>
+    // >::type
     >::type
     >::type;
   #endif
@@ -401,8 +405,8 @@ void AltSignalWorld::InitInstLib() {
   inst_lib->AddInst("If", sgp::lfp_inst_impl::Inst_If<hardware_t, inst_t>, "", {inst_prop_t::BLOCK_DEF});
   inst_lib->AddInst("While", sgp::lfp_inst_impl::Inst_While<hardware_t, inst_t>, "", {inst_prop_t::BLOCK_DEF});
   inst_lib->AddInst("Routine", sgp::lfp_inst_impl::Inst_Routine<hardware_t, inst_t>, "");
-  inst_lib->AddInst("Terminal", sgp::inst_impl::Inst_Terminal<hardware_t, inst_t>, "");
-  inst_lib->AddInst("NegTerminal", sgp::inst_impl::Inst_NegTerminal<hardware_t, inst_t>, "");
+  inst_lib->AddInst("Terminal", sgp::inst_impl::Inst_Terminal<hardware_t, inst_t,
+                                                              std::ratio<1>, std::ratio<-1>>, "");
 
   // If we can use global memory, give programs access. Otherwise, nops.
   if (USE_GLOBAL_MEMORY) {
@@ -431,6 +435,18 @@ void AltSignalWorld::InitInstLib() {
         sgp::inst_impl::Inst_SetRegulator<hardware_t, inst_t>(hw, inst);
       }
     }, "");
+    inst_lib->AddInst("SetRegulator-", [this](hardware_t & hw, const inst_t & inst) {
+      if (KO_REGULATION) {
+        return;
+      } else if (KO_DOWN_REGULATION) {
+        inst_impls::Inst_SetRegulator_KO_DOWN_REG<hardware_t, inst_t, -1>(hw, inst);
+      } else if (KO_UP_REGULATION) {
+        inst_impls::Inst_SetRegulator_KO_UP_REG<hardware_t, inst_t, -1>(hw, inst);
+      } else {
+        sgp::inst_impl::Inst_SetRegulator<hardware_t, inst_t, -1>(hw, inst);
+      }
+    }, "");
+
     inst_lib->AddInst("SetOwnRegulator", [this](hardware_t & hw, const inst_t & inst) {
       if (KO_REGULATION) {
         return;
@@ -442,6 +458,18 @@ void AltSignalWorld::InitInstLib() {
         sgp::inst_impl::Inst_SetOwnRegulator<hardware_t, inst_t>(hw, inst);
       }
     }, "");
+    inst_lib->AddInst("SetOwnRegulator-", [this](hardware_t & hw, const inst_t & inst) {
+      if (KO_REGULATION) {
+        return;
+      } else if (KO_DOWN_REGULATION) {
+        inst_impls::Inst_SetOwnRegulator_KO_DOWN_REG<hardware_t, inst_t, -1>(hw, inst);
+      } else if (KO_UP_REGULATION) {
+        inst_impls::Inst_SetOwnRegulator_KO_UP_REG<hardware_t, inst_t, -1>(hw, inst);
+      } else {
+        sgp::inst_impl::Inst_SetOwnRegulator<hardware_t, inst_t, -1>(hw, inst);
+      }
+    }, "");
+
     inst_lib->AddInst("AdjRegulator", [this](hardware_t & hw, const inst_t & inst) {
       if (KO_REGULATION) {
         return;
@@ -451,6 +479,17 @@ void AltSignalWorld::InitInstLib() {
         inst_impls::Inst_AdjRegulator_KO_UP_REG<hardware_t, inst_t>(hw, inst);
       } else {
         sgp::inst_impl::Inst_AdjRegulator<hardware_t, inst_t>(hw, inst);
+      }
+    }, "");
+    inst_lib->AddInst("AdjRegulator-", [this](hardware_t & hw, const inst_t & inst) {
+      if (KO_REGULATION) {
+        return;
+      } else if (KO_DOWN_REGULATION) {
+        inst_impls::Inst_AdjRegulator_KO_DOWN_REG<hardware_t, inst_t, -1>(hw, inst);
+      } else if (KO_UP_REGULATION) {
+        inst_impls::Inst_AdjRegulator_KO_UP_REG<hardware_t, inst_t, -1>(hw, inst);
+      } else {
+        sgp::inst_impl::Inst_AdjRegulator<hardware_t, inst_t, -1>(hw, inst);
       }
     }, "");
     inst_lib->AddInst("AdjOwnRegulator", [this](hardware_t & hw, const inst_t & inst) {
@@ -464,6 +503,18 @@ void AltSignalWorld::InitInstLib() {
         sgp::inst_impl::Inst_AdjOwnRegulator<hardware_t, inst_t>(hw, inst);
       }
     }, "");
+    inst_lib->AddInst("AdjOwnRegulator-", [this](hardware_t & hw, const inst_t & inst) {
+      if (KO_REGULATION) {
+        return;
+      } else if (KO_DOWN_REGULATION) {
+        inst_impls::Inst_AdjOwnRegulator_KO_DOWN_REG<hardware_t, inst_t, -1>(hw, inst);
+      } else if (KO_UP_REGULATION) {
+        inst_impls::Inst_AdjOwnRegulator_KO_UP_REG<hardware_t, inst_t, -1>(hw, inst);
+      } else {
+        sgp::inst_impl::Inst_AdjOwnRegulator<hardware_t, inst_t, -1>(hw, inst);
+      }
+    }, "");
+
     inst_lib->AddInst("ClearRegulator", [this](hardware_t & hw, const inst_t & inst) {
       if (KO_REGULATION) {
         return;
@@ -486,7 +537,6 @@ void AltSignalWorld::InitInstLib() {
         sgp::inst_impl::Inst_ClearOwnRegulator<hardware_t, inst_t>(hw, inst);
       }
     }, "");
-
     inst_lib->AddInst("SenseRegulator", [this](hardware_t & hw, const inst_t & inst) {
       if (!KO_REGULATION) sgp::inst_impl::Inst_SenseRegulator<hardware_t, inst_t>(hw, inst);
     }, "");
@@ -527,6 +577,10 @@ void AltSignalWorld::InitInstLib() {
     inst_lib->AddInst("Nop-SetOwnRegulator", sgp::inst_impl::Inst_Nop<hardware_t, inst_t>, "");
     inst_lib->AddInst("Nop-AdjRegulator", sgp::inst_impl::Inst_Nop<hardware_t, inst_t>, "");
     inst_lib->AddInst("Nop-AdjOwnRegulator", sgp::inst_impl::Inst_Nop<hardware_t, inst_t>, "");
+    inst_lib->AddInst("Nop-SetRegulator-", sgp::inst_impl::Inst_Nop<hardware_t, inst_t>, "");
+    inst_lib->AddInst("Nop-SetOwnRegulator-", sgp::inst_impl::Inst_Nop<hardware_t, inst_t>, "");
+    inst_lib->AddInst("Nop-AdjRegulator-", sgp::inst_impl::Inst_Nop<hardware_t, inst_t>, "");
+    inst_lib->AddInst("Nop-AdjOwnRegulator-", sgp::inst_impl::Inst_Nop<hardware_t, inst_t>, "");
     inst_lib->AddInst("Nop-ClearRegulator", sgp::inst_impl::Inst_Nop<hardware_t, inst_t>, "");
     inst_lib->AddInst("Nop-ClearOwnRegulator", sgp::inst_impl::Inst_Nop<hardware_t, inst_t>, "");
     inst_lib->AddInst("Nop-SenseRegulator", sgp::inst_impl::Inst_Nop<hardware_t, inst_t>, "");
--- source/reg_ko_instr_impls.h
+++ source/reg_ko_instr_impls.h
@@ -5,13 +5,13 @@ namespace inst_impls {
   /// Non-default instruction: SetRegulator
   /// Number of arguments: 2
   /// Description: Sets the regulator of a tag in the matchbin.
-  template<typename HARDWARE_T, typename INSTRUCTION_T>
+  template<typename HARDWARE_T, typename INSTRUCTION_T, int MULTIPLIER=1>
   void Inst_SetRegulator_KO_UP_REG(HARDWARE_T & hw, const INSTRUCTION_T & inst) {
     emp::vector<size_t> best_fun(hw.GetMatchBin().MatchRaw(inst.GetTag(0), 1));
     if (best_fun.size() == 0) { return; }
     auto & call_state = hw.GetCurThread().GetExecState().GetTopCallState();
     auto & mem_state = call_state.GetMemory();
-    const double new_regulator_val = mem_state.AccessWorking(inst.GetArg(0));
+    const double new_regulator_val = MULTIPLIER * mem_state.AccessWorking(inst.GetArg(0));
     const double old_regulator_val = hw.GetMatchBin().ViewRegulator(best_fun[0]);
     if (new_regulator_val < old_regulator_val) return; // knockout up regulation
     // (+) values down regulate
@@ -19,13 +19,13 @@ namespace inst_impls {
     hw.GetMatchBin().SetRegulator(best_fun[0], new_regulator_val);
   }
 
-  template<typename HARDWARE_T, typename INSTRUCTION_T>
+  template<typename HARDWARE_T, typename INSTRUCTION_T, int MULTIPLIER=1>
   void Inst_SetRegulator_KO_DOWN_REG(HARDWARE_T & hw, const INSTRUCTION_T & inst) {
     emp::vector<size_t> best_fun(hw.GetMatchBin().MatchRaw(inst.GetTag(0), 1));
     if (best_fun.size() == 0) { return; }
     auto & call_state = hw.GetCurThread().GetExecState().GetTopCallState();
     auto & mem_state = call_state.GetMemory();
-    const double new_regulator_val = mem_state.AccessWorking(inst.GetArg(0));
+    const double new_regulator_val = MULTIPLIER * mem_state.AccessWorking(inst.GetArg(0));
     const double old_regulator_val = hw.GetMatchBin().ViewRegulator(best_fun[0]);
     if (new_regulator_val > old_regulator_val) return; // knockout down regulation
     // (+) values down regulate
@@ -37,12 +37,12 @@ namespace inst_impls {
   /// Non-default instruction: SetOwnRegulator
   /// Number of arguments: 2
   /// Description: Sets the regulator of the currently executing function.
-  template<typename HARDWARE_T, typename INSTRUCTION_T>
+  template<typename HARDWARE_T, typename INSTRUCTION_T, int MULTIPLIER=1>
   static void Inst_SetOwnRegulator_KO_UP_REG(HARDWARE_T & hw, const INSTRUCTION_T & inst) {
     auto & call_state = hw.GetCurThread().GetExecState().GetTopCallState();
     auto & mem_state = call_state.GetMemory();
     auto & flow = call_state.GetTopFlow();
-    const double new_regulator_val = mem_state.AccessWorking(inst.GetArg(0));
+    const double new_regulator_val = MULTIPLIER * mem_state.AccessWorking(inst.GetArg(0));
     const double old_regulator_val = hw.GetMatchBin().ViewRegulator(flow.GetMP());
     if (new_regulator_val < old_regulator_val) return; // knockout up regulation
     // (+) values down regulate
@@ -50,12 +50,12 @@ namespace inst_impls {
     hw.GetMatchBin().SetRegulator(flow.GetMP(), new_regulator_val);
   }
 
-  template<typename HARDWARE_T, typename INSTRUCTION_T>
+  template<typename HARDWARE_T, typename INSTRUCTION_T, int MULTIPLIER=1>
   static void Inst_SetOwnRegulator_KO_DOWN_REG(HARDWARE_T & hw, const INSTRUCTION_T & inst) {
     auto & call_state = hw.GetCurThread().GetExecState().GetTopCallState();
     auto & mem_state = call_state.GetMemory();
     auto & flow = call_state.GetTopFlow();
-    const double new_regulator_val = mem_state.AccessWorking(inst.GetArg(0));
+    const double new_regulator_val = MULTIPLIER * mem_state.AccessWorking(inst.GetArg(0));
     const double old_regulator_val = hw.GetMatchBin().ViewRegulator(flow.GetMP());
     if (new_regulator_val > old_regulator_val) return; // knockout down regulation
     // (+) values down regulate
@@ -101,52 +101,52 @@ namespace inst_impls {
 
   /// Non-default instruction: AdjRegulator
   /// Number of arguments: 3
-  template<typename HARDWARE_T, typename INSTRUCTION_T>
+  template<typename HARDWARE_T, typename INSTRUCTION_T, int MULTIPLIER=1>
   static void Inst_AdjRegulator_KO_UP_REG(HARDWARE_T & hw, const INSTRUCTION_T & inst) {
     // const State & state = hw.GetCurState();
     emp::vector<size_t> best_fun = hw.GetMatchBin().MatchRaw(inst.GetTag(0), 1);
     if (!best_fun.size()) return;
     auto & call_state = hw.GetCurThread().GetExecState().GetTopCallState();
     auto & mem_state = call_state.GetMemory();
-    const double adj = mem_state.AccessWorking(inst.GetArg(0));
+    const double adj = MULTIPLIER * mem_state.AccessWorking(inst.GetArg(0));
     if (adj < 0) return; // knockout up regulation
     hw.GetMatchBin().AdjRegulator(best_fun[0], adj);
   }
 
   /// Non-default instruction: AdjRegulator
   /// Number of arguments: 3
-  template<typename HARDWARE_T, typename INSTRUCTION_T>
+  template<typename HARDWARE_T, typename INSTRUCTION_T, int MULTIPLIER=1>
   static void Inst_AdjRegulator_KO_DOWN_REG(HARDWARE_T & hw, const INSTRUCTION_T & inst) {
     // const State & state = hw.GetCurState();
     emp::vector<size_t> best_fun = hw.GetMatchBin().MatchRaw(inst.GetTag(0), 1);
     if (!best_fun.size()) return;
     auto & call_state = hw.GetCurThread().GetExecState().GetTopCallState();
     auto & mem_state = call_state.GetMemory();
-    const double adj = mem_state.AccessWorking(inst.GetArg(0));
+    const double adj = MULTIPLIER * mem_state.AccessWorking(inst.GetArg(0));
     if (adj > 0) return;
     hw.GetMatchBin().AdjRegulator(best_fun[0], adj);
   }
 
   /// Non-default instruction: AdjOwnRegulator
   /// Number of arguments: 3
-  template<typename HARDWARE_T, typename INSTRUCTION_T>
+  template<typename HARDWARE_T, typename INSTRUCTION_T, int MULTIPLIER=1>
   static void Inst_AdjOwnRegulator_KO_UP_REG(HARDWARE_T & hw, const INSTRUCTION_T & inst) {
     auto & call_state = hw.GetCurThread().GetExecState().GetTopCallState();
     auto & mem_state = call_state.GetMemory();
     auto & flow = call_state.GetTopFlow();
-    const double adj = mem_state.AccessWorking(inst.GetArg(0));
+    const double adj = MULTIPLIER * mem_state.AccessWorking(inst.GetArg(0));
     if (adj < 0) return;
     hw.GetMatchBin().AdjRegulator(flow.GetMP(), adj);
   }
 
   /// Non-default instruction: AdjOwnRegulator
   /// Number of arguments: 3
-  template<typename HARDWARE_T, typename INSTRUCTION_T>
+  template<typename HARDWARE_T, typename INSTRUCTION_T, int MULTIPLIER=1>
   static void Inst_AdjOwnRegulator_KO_DOWN_REG(HARDWARE_T & hw, const INSTRUCTION_T & inst) {
     auto & call_state = hw.GetCurThread().GetExecState().GetTopCallState();
     auto & mem_state = call_state.GetMemory();
     auto & flow = call_state.GetTopFlow();
-    const double adj = mem_state.AccessWorking(inst.GetArg(0));
+    const double adj = MULTIPLIER * mem_state.AccessWorking(inst.GetArg(0));
     if (adj > 0) return;
     hw.GetMatchBin().AdjRegulator(flow.GetMP(), adj);
   }
