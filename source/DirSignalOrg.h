053b0a5702b74c9db2560a355d3e4d32e1e9faeb
Alex Lalejini <amlalejini@gmail.com>
2020-03-13 12:30:57 -0400

    Cleanup DirSignalOrg.h

--- source/DirSignalOrg.h
+++ source/DirSignalOrg.h
@@ -3,11 +3,7 @@
 
 #include <tuple>
 #include <algorithm>
-
-// New signalgp includes
 #include "hardware/SignalGP/utils/LinearFunctionsProgram.h"
-
-// local includes
 #include "mutation_utils.h"
 
 template<typename TAG_T, typename INST_ARG_T>
@@ -41,23 +37,19 @@ public:
 
     const program_t & GetProgram() const { return program; }
     program_t & GetProgram() { return program; }
-
   };
 
-  // struct TrialProfile {
-  //   double score;
-  // };
-
+  /// Phenotype definition for directional signal task organisms.
   struct DirSigPhenotype {
     using this_t = DirSigPhenotype;
 
-    emp::vector<double> test_scores;
-    emp::vector<size_t> test_ids;
-    double aggregate_score=0.0;
+    emp::vector<double> test_scores;  ///< Scores on tests. Each test corresponds to a different signal sequence.
+    emp::vector<size_t> test_ids;     ///< Sequence IDs of tests (aligned with test_scores)
+    double aggregate_score=0.0;       ///< Aggregate score across all signal sequences tested on (i.e., sum(test_scores))
 
     void Reset(size_t tests=1) {
       aggregate_score=0.0;         // Reset aggregate score.
-      test_scores.resize(tests); // Make sure trial scores is right size.
+      test_scores.resize(tests);   // Make sure trial scores is right size.
       test_ids.resize(tests);
       std::fill(test_scores.begin(), test_scores.end(), 0); // Fill with zeroes.
       std::fill(test_ids.begin(), test_ids.end(), 0);
@@ -90,7 +82,6 @@ public:
     }
 
     double GetAggregateScore() const { return aggregate_score; }
-
   };
 
 protected:
