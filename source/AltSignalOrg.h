d2f4b83d23ce4baf414ff254034f9ebc0b0999bc
Alex Lalejini <amlalejini@gmail.com>
2020-03-13 11:23:24 -0400

    AltSignalOrg.h cleanup

--- source/AltSignalOrg.h
+++ source/AltSignalOrg.h
@@ -2,19 +2,16 @@
 #define ALT_SIGNAL_DIGITAL_ORGANISM_H
 
 #include <tuple>
-
-// New signalgp includes
 #include "hardware/SignalGP/utils/LinearFunctionsProgram.h"
-
-// local includes
 #include "mutation_utils.h"
 
+/// Organism class definition for the repeated signal task.
 template<typename TAG_T, typename INST_ARG_T>
 class AltSignalOrganism {
 public:
   struct AltSignalGenome;
 
-  using program_t = sgp::LinearFunctionsProgram<TAG_T, INST_ARG_T>;
+  using program_t = sgp::LinearFunctionsProgram<TAG_T, INST_ARG_T>; ///< SignalGP version with explicit modules, each with a linear sequence of instructions.
   using genome_t = AltSignalGenome;
 
   struct AltSignalGenome {
@@ -37,11 +34,11 @@ public:
 
     const program_t & GetProgram() const { return program; }
     program_t & GetProgram() { return program; }
-
   };
 
+  /// Phenotype for repeated signal task organisms.
   struct AltSignalPhenotype {
-    double resources_consumed=0.0;
+    double resources_consumed=0.0;  ///< I.e., fitness
     size_t correct_resp_cnt=0;      ///< How many correct responses did organism achieve during evaluation?
     size_t no_resp_cnt=0;           ///< How many 'no response' updates did organism have during evaluation?
 
@@ -72,7 +69,7 @@ protected:
   AltSignalPhenotype phenotype;
   genome_t genome;
 
-  std::unordered_map<std::string, int> mutations; ///< Mutations from parent.
+  std::unordered_map<std::string, int> mutations; ///< Program mutations away from parent.
 
 public:
   AltSignalOrganism(const genome_t & g)
@@ -95,7 +92,6 @@ public:
       pair.second = 0;
     }
   }
-
 };
 
 #endif
