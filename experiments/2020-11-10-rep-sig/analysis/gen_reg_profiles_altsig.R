46c9f83392cbc86926febf915b43105fdb96636a
Alexander Lalejini <amlalejini@gmail.com>
2020-11-25 17:48:03 -0500

    Update 2020-11-10 analysis scripts

--- experiments/2020-11-10-rep-sig/analysis/gen_reg_profiles_altsig.R
+++ experiments/2020-11-10-rep-sig/analysis/gen_reg_profiles_altsig.R
@@ -29,15 +29,15 @@ get_con <- function(reg, mem) {
   }
 }
 # Specify experimental condition for each datum.
-max_fit_org_data$condition <- mapply(get_con, 
-                                     max_fit_org_data$USE_FUNC_REGULATION, 
+max_fit_org_data$condition <- mapply(get_con,
+                                     max_fit_org_data$USE_FUNC_REGULATION,
                                      max_fit_org_data$USE_GLOBAL_MEMORY)
 max_fit_org_data$condition <- factor(max_fit_org_data$condition,
                                      levels=c("regulation", "memory", "none", "both"))
 for (trace_id in trace_ids) {
   print(trace_id)
   org_info <- filter(max_fit_org_data, SEED==trace_id)
-  
+
   if (nrow(org_info) == 0) {
     next
   }
@@ -48,17 +48,17 @@ for (trace_id in trace_ids) {
   if (is_sol == "0") {
     next
   }
-  
-  trace_file <- paste(data_dir, 
-                      "traces/trace_update-", generation,
+
+  trace_file <- paste(data_dir,
+                      "reg-traces/trace-reg_update-", generation,
                       "_run-id-", trace_id, ".csv", sep="")
   trace_data <- read.csv(trace_file, na.strings="NONE")
   # Do some trace data clean up/add extra fields.
   trace_data$similarity_score <- 1 - trace_data$match_score
   trace_data$triggered <- (trace_data$env_signal_closest_match == trace_data$module_id) & (trace_data$cpu_step == "0")
   trace_data$is_running <- trace_data$is_running > 0 | trace_data$triggered | trace_data$is_cur_responding_function == "1"
-  
-  # correct response ids  
+
+  # correct response ids
   response_time_steps <- levels(factor(filter(trace_data, is_cur_responding_function=="1")$time_step))
   responses_by_env_update <- list()
   for (t in response_time_steps) {
@@ -66,7 +66,7 @@ for (trace_id in trace_ids) {
     if (env_update %in% names(responses_by_env_update)) {
       if (as.integer(t) > as.integer(responses_by_env_update[env_update])) {
         responses_by_env_update[env_update] = t
-      } 
+      }
     } else {
       responses_by_env_update[env_update] = t
     }
@@ -74,19 +74,19 @@ for (trace_id in trace_ids) {
   # for (env_update in responses_by_env_update) {
   #   print(env_update)
   # }
-  
+
   # Build a list of modules that were triggered
   triggered_ids <- levels(factor(filter(trace_data, triggered==TRUE)$module_id))
   response_ids <- levels(factor(filter(trace_data, is_cur_responding_function=="1")$module_id))
-  
-  trace_data$is_ever_active <- trace_data$is_ever_active=="1" | 
-    trace_data$is_running | 
-    trace_data$module_id %in% triggered_ids | 
+
+  trace_data$is_ever_active <- trace_data$is_ever_active=="1" |
+    trace_data$is_running |
+    trace_data$module_id %in% triggered_ids |
     trace_data$module_id %in% response_ids
-  
+
   trace_data$is_cur_responding_function <- trace_data$is_cur_responding_function=="1" &
     trace_data$time_step %in% responses_by_env_update
-  
+
   categorize_reg_state <- function(reg_state) {
     if (reg_state == 0) {
       return("neutral")
@@ -100,8 +100,8 @@ for (trace_id in trace_ids) {
   }
   trace_data$regulator_state_simplified <- mapply(categorize_reg_state,
                                                   trace_data$regulator_state)
-  
-  
+
+
   # Extract only rows that correspond with modules that were active during evaluation.
   active_data <- filter(trace_data, is_ever_active==TRUE)
   # Do some work to have module ids appear in a nice order along axis.
@@ -114,35 +114,35 @@ for (trace_id in trace_ids) {
   }
   active_data$mod_id_x_pos <- mapply(get_module_x_pos, active_data$module_id)
   trace_data <- active_data # omit all non-active modules from visualization
-  
+
   title <- paste("Condition: ",condition, "; # Envs: ", num_envs, ";\nScore: ", score, "; Solution? ", is_sol , sep="")
   out_name <- paste(trace_img_dump_dir, "trace-id-", trace_id, "-regulator-state.pdf", sep="")
-  ggplot(trace_data, 
-         aes(x=mod_id_x_pos, 
-             y=time_step, 
+  ggplot(trace_data,
+         aes(x=mod_id_x_pos,
+             y=time_step,
              fill=regulator_state_simplified)) +
     scale_fill_viridis(name="State:",
                        limits=c("promoted", "neutral", "repressed"),
                        labels=c("Promoted", "Neutral", "Repressed"),
-                       discrete=TRUE, 
+                       discrete=TRUE,
                        direction=-1) +
     scale_x_discrete(name="Function ID",
                      limits=seq(0, length(active_module_ids)-1, 1),
                      labels=active_module_ids) +
     ylab("Time Step") +
     # Background
-    geom_tile(color="white", 
-              size=0.2, 
-              width=1, 
+    geom_tile(color="white",
+              size=0.2,
+              width=1,
               height=1) +
     # Module is-running highlights
-    geom_tile(data=filter(trace_data, is_running==TRUE), 
-              color="black", 
-              width=1, 
-              height=1, 
+    geom_tile(data=filter(trace_data, is_running==TRUE),
+              color="black",
+              width=1,
+              height=1,
               size=0.8) +
     # Environment delimiters
-    geom_hline(yintercept=filter(trace_data, cpu_step==0)$time_step - 0.5, 
+    geom_hline(yintercept=filter(trace_data, cpu_step==0)$time_step - 0.5,
                size=1) +
     # Draw points on triggered modules
     geom_point(data=filter(trace_data, triggered==TRUE),
@@ -157,11 +157,11 @@ for (trace_id in trace_ids) {
           axis.text.x = element_text(size=10)) +
     ggtitle(title) +
     ggsave(out_name, height=7, width=5)
-  
+
   out_name <- paste(trace_img_dump_dir, "trace-id-", trace_id, "-similarity-score.pdf", sep="")
-  ggplot(trace_data, 
-         aes(x=mod_id_x_pos, 
-             y=time_step, 
+  ggplot(trace_data,
+         aes(x=mod_id_x_pos,
+             y=time_step,
              fill=similarity_score)) +
     scale_fill_viridis(option="plasma",
                        name="Score:  ",
@@ -172,15 +172,15 @@ for (trace_id in trace_ids) {
                      limits=seq(0, length(active_module_ids)-1, 1),
                      labels=active_module_ids) +
     # Background
-    geom_tile(color="white", 
-              size=0.2, 
-              width=1, 
+    geom_tile(color="white",
+              size=0.2,
+              width=1,
               height=1) +
     # Module is-running highlights
-    geom_tile(data=filter(trace_data, is_running==TRUE | triggered==TRUE), 
-              color="black", 
-              width=1, 
-              height=1, 
+    geom_tile(data=filter(trace_data, is_running==TRUE | triggered==TRUE),
+              color="black",
+              width=1,
+              height=1,
               size=0.8) +
     # Environment delimiters
     geom_hline(yintercept=filter(trace_data, cpu_step==0)$time_step-0.5, size=1) +
