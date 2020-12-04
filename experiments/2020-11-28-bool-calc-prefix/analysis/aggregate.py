c27a9a2a14ad694ef2b00f9d211f3425a341e059
Alexander Lalejini <amlalejini@gmail.com>
2020-12-03 20:13:19 -0500

    fix test case indexing

--- experiments/2020-11-28-bool-calc-prefix/analysis/aggregate.py
+++ experiments/2020-11-28-bool-calc-prefix/analysis/aggregate.py
@@ -266,7 +266,7 @@ def main():
         num_modules = int(org["num_modules"])
 
         modules_active_ever = set()
-        modules_run_by_test = {testcase:set() for testcase in testcase_ids}
+        modules_run_by_test = {int(testcase):set() for testcase in testcase_ids}
 
         modules_present_by_step = [[0 for m in range(0, num_modules)] for i in range(0, len(time_steps))]
         modules_active_by_step = [[0 for m in range(0, num_modules)] for i in range(0, len(time_steps))]
