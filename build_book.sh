14958a3fe5c3e7d165039b2cf6e7aef800bc2127
Alex Lalejini <amlalejini@gmail.com>
2021-03-03 16:02:32 -0500

    update book build script

--- build_book.sh
+++ build_book.sh
@@ -2,10 +2,5 @@
 
 set -ev
 
-rm -rf ./supplemental/
-rm -rf ./supplemental_files/
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-mv _book supplemental
-mv _bookdown_files supplemental_files
-
